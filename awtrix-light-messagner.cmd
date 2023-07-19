34dc89ba663b673eb8747e4c50e598bf0f43df54
chz87 <63285097+chz87@users.noreply.github.com>
2023-07-19 10:01:10 +0200

    Update awtrix-light-messagner.cmd

--- awtrix-light-messagner.cmd
+++ awtrix-light-messagner.cmd
@@ -1,1 +1,1 @@
-C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe  -WindowStyle hidden -command "& '.\Message4Schatz.ps1'"
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe  -WindowStyle hidden -command "& '.\awtrix-light-messagner.ps1'"
